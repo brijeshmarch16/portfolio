17f8ee508f61bafa1c8b18a385e0df9712944720
Brijeshkumar Yadav <brijeshkumaryadav2026@gmail.com>
2026-01-05 08:43:10 +0530

    feat: Integrate Vercel Analytics into the layout and update package dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import { Archivo_Black, Space_Grotesk } from "next/font/google";
 import { Navbar } from "@/features/global/components/navbar";
 import { ThemeProvider } from "@/features/global/components/theme-provider";
@@ -42,6 +43,7 @@ export default function RootLayout({
         >
           <Navbar items={navItems} />
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
